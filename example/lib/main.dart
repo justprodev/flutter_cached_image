59f6a6598d651b9ce4674c55b50a3160b8470cbb
Alexander Yemelyanov <alex@justprodev.com>
2024-08-25 00:48:13 +0500

    added loading animation; added meta;

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo images feed',
+      title: 'Flutter example app for cached_image package',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
